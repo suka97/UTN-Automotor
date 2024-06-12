84de1ce5ccc1729056e7386578d41a196c6cd1b5
suka97 <andressukanec@hotmail.com>
2024-06-11 21:11:50 -0300

    2 separate tasks

--- automotor-tp1-bmef/main/main.c
+++ automotor-tp1-bmef/main/main.c
@@ -10,12 +10,19 @@
 #define MY_GPIO_LED             2
 #define MY_GPIO_BUTTON          0   // Uses external pull-up resistor
 #define MY_ADC1_CHAN_SPEED      ADC_CHANNEL_0
+#define MY_ADC1_CHAN_BMEF       ADC_CHANNEL_3
+#define MY_ADC1_CHAN_CURRENT    ADC_CHANNEL_6
+
 #define MY_PWM_FREQ             1000
 
 #define LEDC_MODE               LEDC_HIGH_SPEED_MODE
+#define TASKS_STACK_SIZE        2048
 
 
 adc_oneshot_unit_handle_t adc1_handle;
+uint32_t pwm_duty = 0;
+bool pwm_enabled = false;
+int adc_raw_speed, adc_raw_bmef, adc_raw_current;
 
 
 esp_err_t init_gpios(void) {
@@ -46,6 +53,8 @@ esp_err_t init_adc(void) {
         .atten = ADC_ATTEN_DB_12,
     };
     ESP_ERROR_CHECK( adc_oneshot_config_channel(adc1_handle, MY_ADC1_CHAN_SPEED, &config) );
+    ESP_ERROR_CHECK( adc_oneshot_config_channel(adc1_handle, MY_ADC1_CHAN_BMEF, &config) );
+    ESP_ERROR_CHECK( adc_oneshot_config_channel(adc1_handle, MY_ADC1_CHAN_CURRENT, &config) );
 
     return ESP_OK;
 }
@@ -78,27 +87,59 @@ esp_err_t init_pwm(void) {
 }
 
 
+void vTaskReadInputs(void *pvParameters) {
+    while(1) {
+        // Read ADC
+        ESP_ERROR_CHECK( adc_oneshot_read(adc1_handle, MY_ADC1_CHAN_SPEED, &adc_raw_speed) );
+        // Button turn pwm on/off
+        if (gpio_get_level(MY_GPIO_BUTTON) == 0) {
+            if (pwm_enabled) {
+                pwm_enabled = false;
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_0, 0);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_0);
+            } else {
+                pwm_enabled = true;
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_0, adc_raw_speed);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_0);
+            }
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+
+void vTaskDebug(void *pvParameters) {
+    while(1) {
+        printf("/*%d*/\n", adc_raw_speed);
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+
+TaskHandle_t xHandleReadInputs = NULL;
+TaskHandle_t xHandleDebug = NULL;
+
+esp_err_t init_tasks(void) {
+    xTaskCreate( vTaskReadInputs, "vTaskReadInputs", TASKS_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandleReadInputs );
+    configASSERT( xHandleReadInputs );
+    xTaskCreate( vTaskDebug, "vTaskDebug", TASKS_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandleDebug );
+    configASSERT( xHandleDebug );
+
+    return ESP_OK;
+}
+
+
 void app_main(void) {
     ESP_ERROR_CHECK( init_gpios() );
     ESP_ERROR_CHECK( init_adc() );
     ESP_ERROR_CHECK( init_pwm() );
 
+    ESP_ERROR_CHECK( init_tasks() );
+
     while(1) {
-        // if (gpio_get_level(GPIO_BUTTON) == 0) {
-        //     gpio_set_level(MY_GPIO_LED, 1);
-        // } else {
-        //     gpio_set_level(MY_GPIO_LED, 0);
-        // }
-        // vTaskDelay(10 / portTICK_PERIOD_MS);
-
-        // Read ADC
-        int adc_raw;
-        ESP_ERROR_CHECK( adc_oneshot_read(adc1_handle, MY_ADC1_CHAN_SPEED, &adc_raw) );
-        printf("/*%d*/\r\n", adc_raw);
-        // Set PWM
-        ESP_ERROR_CHECK( ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_0, adc_raw) );
-        ESP_ERROR_CHECK( ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_0) );
-
-        vTaskDelay(50 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // vTaskSuspend(NULL); // Suspend main task
     }
 }
