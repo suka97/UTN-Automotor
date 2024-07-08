9ab0e9db1044e5721d6e60e7d3bbd9c9733fb29a
suka97 <andressukanec@hotmail.com>
2024-07-08 00:08:32 -0300

    Master sending with parity

--- automotor-tp2-communication/lin-master/main/main.c
+++ automotor-tp2-communication/lin-master/main/main.c
@@ -11,6 +11,35 @@ static const char *TAG = "UART TEST";
 #define MY_UART_PORT_NUM       UART_NUM_1
 #define MY_UART_BAUD_RATE      115200
 #define MY_UART_BUFFER_SIZE    1024
+#define MY_UART_TIMEOUT_MS     20
+
+char rx_buffer[MY_UART_BUFFER_SIZE];
+char tx_buffer[MY_UART_BUFFER_SIZE];
+
+
+// Calculate LIN PID Parity bits
+uint8_t _lin_calc_pid(uint8_t id) {
+    // Ensure ID is within 6-bit range
+    id &= 0x3F;
+    // Calculate P0: Parity of bits 0, 1, 2, and 4 (odd parity)
+    uint8_t p0 = ((id >> 0) & 1) ^ ((id >> 1) & 1) ^ ((id >> 2) & 1) ^ ((id >> 4) & 1);
+    // Calculate P1: Parity of bits 1, 3, 4, and 5 (odd parity)
+    uint8_t p1 = ~(((id >> 1) & 1) ^ ((id >> 3) & 1) ^ ((id >> 4) & 1) ^ ((id >> 5) & 1)) & 1;
+    // Combine ID and parity bits into a PID
+    uint8_t pid = (p0 << 6) | (p1 << 7) | id;
+    return pid;
+}
+
+
+esp_err_t lin_send(uint8_t id) {
+    uint8_t pid = _lin_calc_pid(id);
+
+    // TODO: Implement SyncBreack with additional GPIO
+    uart_write_bytes(MY_UART_PORT_NUM, "\x55", 1); // Sync Byte
+    uart_write_bytes(MY_UART_PORT_NUM, &pid, 1); // Sync Byte
+
+    return ESP_OK;
+}
 
 
 esp_err_t init_uart(void) {
@@ -24,7 +53,7 @@ esp_err_t init_uart(void) {
     };
     int intr_alloc_flags = 0;
 
-    ESP_ERROR_CHECK(uart_driver_install(MY_UART_PORT_NUM, MY_UART_BUFFER_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_driver_install(MY_UART_PORT_NUM, MY_UART_BUFFER_SIZE, 0, 0, NULL, intr_alloc_flags));
     ESP_ERROR_CHECK(uart_param_config(MY_UART_PORT_NUM, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(MY_UART_PORT_NUM, 10, 9, -1, -1));
 
@@ -33,35 +62,32 @@ esp_err_t init_uart(void) {
 
 
 void vTaskUartRx(void *pvParameters) {
-    uint8_t *data = (uint8_t *) malloc(MY_UART_BUFFER_SIZE);
     while(1) {
-        int len = uart_read_bytes(MY_UART_PORT_NUM, data, MY_UART_BUFFER_SIZE, 20 / portTICK_PERIOD_MS);
+        int len = uart_read_bytes(MY_UART_PORT_NUM, rx_buffer, MY_UART_BUFFER_SIZE, MY_UART_TIMEOUT_MS/portTICK_PERIOD_MS);
         if (len > 0) {
-            ESP_LOGI(TAG, "Read %d bytes: %s", len, data);
+            ESP_LOGI(TAG, "Read %d bytes: %s", len, rx_buffer);
         }
     }
-    free(data);
-    vTaskDelete(NULL);
 }
 
 
 void vTaskUartTx(void *pvParameters) {
     int counter = 0;
-    const uint8_t buffersize = 32;
-    char data[buffersize];
     while(1) {
-        ESP_LOGI(TAG, "Sending counter: %d", counter);
-        sprintf(data, "Counter: %d\n", counter++);
-        uart_write_bytes(MY_UART_PORT_NUM, data, buffersize);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        for ( counter = 0 ; counter < 10 ; counter++ ) {
+            ESP_LOGI(TAG, "Sending id: %d", counter);
+            ESP_ERROR_CHECK( lin_send(counter) );
+
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
     }
 }
 
 
 esp_err_t init_tasks(void) {
     ESP_LOGI(TAG, "Starting tasks");
-    // xTaskCreate( vTaskUartRx, "vTaskUartRx", TASKS_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
     xTaskCreate( vTaskUartTx, "vTaskUartTx", TASKS_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
+    // xTaskCreate( vTaskUartRx, "vTaskUartRx", TASKS_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
 
     return ESP_OK;
 }
